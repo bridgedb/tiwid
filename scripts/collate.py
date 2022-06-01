7d7ae54b7ee76c820c713a42f2c2dcf7a1deafa5
Egon Willighagen <egon.willighagen@gmail.com>
2022-06-02 00:34:00 +0200

    Replaced CSV with TSV (#13)
    * Replaced CVS with TSV
    
    * Better error reporting
    
    * Only TSV now
    
    * Report also the offending line
    
    * Don't strip all whitespace, which includes TABs
    
    * Further cleanup
    
    * Update test_integrity.py
    
    * Remove trailing whitespace
    
    * Update test_integrity.py
    
    Co-authored-by: Charles Tapley Hoyt <cthoyt@gmail.com>

--- scripts/collate.py
+++ scripts/collate.py
@@ -1,6 +1,5 @@
 """Run this script to collate the data."""
 
-from itertools import chain
 from pathlib import Path
 
 HERE = Path(__file__).parent.resolve()
@@ -15,12 +14,11 @@ HEADER = ["#prefix", "did", "when", "nextofkin"]
 def main():
     """Collate all files together."""
     rows = []
-    for path in chain(DATA.glob("*.csv"), DATA.glob("*.tsv")):
+    for path in DATA.glob("*.tsv"):
         with path.open() as file:
-            header = next(file)
-            sep = "," if "," in header else "\t"
+            _header = next(file)
             for line in file:
-                dead_id, when, alt_id = line.strip().split(sep)
+                dead_id, when, alt_id = line.strip().split("\t")
                 rows.append((path.stem, dead_id, when, alt_id))
 
     rows = sorted(rows)
--- tests/test_integrity.py
+++ tests/test_integrity.py
@@ -10,7 +10,6 @@ import bioregistry
 HERE = Path(__file__).parent.resolve()
 ROOT = HERE.parent.resolve()
 DATA = ROOT.joinpath("data")
-EXTENSION = "csv"
 HEADER = ["#did", "when", "nextofkin"]
 N_COLUMNS = len(HEADER)
 DATE_RE = re.compile("^\\d{4}-\\d{2}-\\d{2}")
@@ -21,12 +20,7 @@ class IntegrityTestCase(unittest.TestCase):
 
     def setUp(self) -> None:
         """Set up the test case by finding all CSV and TSV files."""
-        self.paths = list(
-            chain(
-                DATA.glob("*.csv"),
-                DATA.glob("*.tsv"),
-            )
-        )
+        self.paths = list(DATA.glob("*.tsv"))
 
     def test_path_names(self):
         """Check all file names use standard prefixes."""
@@ -34,26 +28,45 @@ class IntegrityTestCase(unittest.TestCase):
             with self.subTest(name=path.name):
                 stem = path.stem
                 norm_stem = bioregistry.normalize_prefix(stem)
-                self.assertEqual(norm_stem, stem, msg="unnormalized file name: path")
+                self.assertEqual(
+                    norm_stem, stem, msg=f"unnormalized file name: {path.name}"
+                )
 
     def test_csv_integrity(self):
         """Test all files have the right columns."""
         for path in self.paths:
-            pattern = bioregistry.get_pattern(path.stem)
+            prefix = path.stem
+            pattern = bioregistry.get_pattern(prefix)
             self.assertIsNotNone(pattern)
             pattern_re = re.compile(pattern)
 
             with self.subTest(name=path.name), path.open() as file:
-                sep = "," if path.suffix == ".csv" else "\t"
-                lines = (line.strip().split(sep) for line in file)
-                header = next(lines)
+                header = next(file).strip("\n").split("\t")
                 self.assertEqual(HEADER, header)
-                for i, line in enumerate(lines, start=2):
+                for i, line in enumerate(file, start=2):
                     with self.subTest(name=path.name, line=i):
-                        self.assertEqual(N_COLUMNS, len(line))
-                        old_id, date, new_id = line
+                        line = line.strip("\n")
+                        self.assertEqual(
+                            line.strip(" "),
+                            line,
+                            msg=f"{path.name} had trailing whitespace on line {i}",
+                        )
+                        try:
+                            old_id, date, new_id = line.split("\t")
+                        except ValueError:
+                            self.fail(
+                                f"{path.name} had wrong number of columns on line {i}"
+                            )
                         if date:
                             self.assertRegex(date, DATE_RE)
-                        self.assertRegex(old_id, pattern_re)
+                        self.assertRegex(
+                            old_id,
+                            pattern_re,
+                            msg=f"{path.name} line {i} had invalid did",
+                        )
                         if new_id:
-                            self.assertRegex(new_id, pattern_re)
+                            self.assertRegex(
+                                new_id,
+                                pattern_re,
+                                msg=f"{path.name} line {i} had invalid nextofkin",
+                            )
